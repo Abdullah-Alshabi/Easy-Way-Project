3ceaf2d58d8a5b095ad4b92c35ca88ffc93e0e73
ziyad <ziyad@almutaiiri@gmail.com>
2025-04-24 05:18:03 +0300

    .

--- app/src/main/java/com/example/fitness/MainActivity.java
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -12,17 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Login;
+    private Button Login , Signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Login = findViewById(R.id.Login);
+        Signin = findViewById(R.id.Signin);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Log_in.class);
+                startActivity(intent);
+            }
+        });
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, Sign_up.class);
                 startActivity(intent);
             }
         });
