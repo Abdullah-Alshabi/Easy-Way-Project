15f1a4a0cc05d5f2821895edbd63e6cd373e432f
Fahad Alkaridis <431109379>
2025-04-26 21:43:17 +0300

    Done!

--- app/src/main/java/com/example/fitness/Log_in.java
+++ app/src/main/java/com/example/fitness/Log_in.java
@@ -1,6 +1,11 @@
 package com.example.fitness;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,23 @@ public class Log_in extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView signUpText = findViewById(R.id.textView4);
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the sign-up activity
+                Intent intent = new Intent(Log_in.this, Sign_up.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView backArrow = (ImageView)findViewById(R.id.back_arrow);
+        backArrow.setOnClickListener(v -> onBackPressed());
     }
 }
